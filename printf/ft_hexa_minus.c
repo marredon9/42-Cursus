5394d6443e657ec733695eeb61b7c92b98c7a367
marina-TUF <marredon1999@gmail.com>
2023-09-12 16:39:39 +0200

    printfe

---
+++ printf/ft_hexa_minus.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hexa_mayus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marredon <marredon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 13:03:02 by marredon          #+#    #+#             */
+/*   Updated: 2023/09/12 13:05:57 by marredon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	puthexa_long(char *bstr, unsigned long long n, int c)
+{
+	unsigned long long	b;
+
+	b = ft_strlen(bstr);
+	if (n >= b)
+	{
+		c = puthexa_long (bstr, n / b, n);
+		if (c == -1)
+			return (-1);
+		if (write (1, &bstr[n % b], 1) == -1)
+			return (-1);
+		c++;
+	}
+	else if (n < b)
+	{
+		if (write (1, &bstr[n], 1) == -1)
+			return (-1);
+		c++;
+	}
+	return (c);
+}
+
+static int	puthexa_uns(char *bstr, unsigned int n, int c)
+{
+	unsigned int	b;
+
+	b = ft_strlen(bstr);
+	if (n >= b)
+	{
+		c = puthexa_uns (bstr, n / b, n);
+		if (c == -1)
+			return (-1);
+		if (write (1, &bstr[n % b], 1) == -1)
+			return (-1);
+		c++;
+	}
+	else if (n < b)
+	{
+		if (write (1, &bstr[n], 1) == -1)
+			return (-1);
+		c++;
+	}
+	return (c);
+}
+
+int	ft_hexa_mayus(int n)
+{
+	int		let;
+
+	let = 0;
+	if (n >= 0)
+	{
+		let = puthexa_long;
+	}
+}
+/* Cooment */
